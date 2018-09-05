4cc5debe365b80e948733f90f6c49d6a0faa6757
desmondmm <mutsakaradesmond@gmail.com>
2018-09-06 00:50:33 +0200

    Added functionality to handle location changes

--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,27 @@ namespace TravelRecordApp
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+            locator.PositionChanged += Locator_PositionChanged;
+            await locator.StartListeningAsync(new TimeSpan(0), 100);
+
+            var position = await locator.GetPositionAsync();
+
+            var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+            var span = new Xamarin.Forms.Maps.MapSpan(center, 2, 2);
+            LocationsMap.MoveToRegion(span);
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            var center = new Xamarin.Forms.Maps.Position(e.Position.Latitude, e.Position.Longitude);
+            var span = new Xamarin.Forms.Maps.MapSpan(center, 2, 2);
+            LocationsMap.MoveToRegion(span);
+        }
+    }
 }
