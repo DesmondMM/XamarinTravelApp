128eb18c7a5ddcd3e56f9b7407f29d7a6556ae5e
desmondmm <mutsakaradesmond@gmail.com>
2018-09-06 16:23:31 +0200

    Added url to connect to rest

---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelRecordApp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret=CLIENT_SECRET&v={4}";
+        public const string CLIENT_ID = "2LQ2VV1EKXXR30AJASO4MSGQFED52PKCJZVYNUZTRH5X3OJI";
+        public const string CLIENT_SECRET = "J2DUKNATZVINKQKMU5SIV5MYSW2V3JZWZOQPO00NR10HANQL";
+    }
+}
